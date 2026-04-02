658f72a067e175bffdb53e3c2c04ca53f5886cd3
Praveen Kumar <praveenshakya441@gmail.com>
2026-04-02 14:06:31 +0530

    Added basic server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
