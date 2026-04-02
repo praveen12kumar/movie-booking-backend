4b6a92984c276c03925af14eafe6aa538dcb7a09
Praveen Kumar <praveenshakya441@gmail.com>
2026-04-02 14:35:14 +0530

    connect with mongodb

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
+const { PORT } = require("./src/config");
+const connectDB = require("./src/config/db");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+  connectDB();
 });
